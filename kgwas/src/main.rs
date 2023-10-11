456995fa87ba677491ec52a8faa63393c669c929
Zhang Yixing <zyxjh21@126.com>
2023-10-11 16:15:23 +0800

    undate main.rs , add tovcf subcommand

--- kgwas/src/main.rs
+++ kgwas/src/main.rs
@@ -4,6 +4,9 @@ use env_logger::fmt::Target;
 use env_logger::Builder;
 use log::LevelFilter;
 use std::collections::HashSet;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
 use std::io::Write;
 mod merge;
 
@@ -40,6 +43,15 @@ enum Subcli {
         #[arg(short = 'o', long = "output", default_value = "filter")]
         prefix: String,
     },
+    /// kmers_table to vcf
+    tovcf {
+        /// kmers_table
+        #[arg(short = 'k', long = "ktable", required = true)]
+        ktable: String,
+        /// prefix of output file
+        #[arg(short = 'o', long = "output", default_value = "table")]
+        prefix: String,
+    },
 }
 
 #[warn(unreachable_patterns)]
@@ -74,9 +86,74 @@ fn main() {
             for i in a.iter() {
                 all_samples.push(merge::filetohash(i, &mut nodes))
             }
-            log::info!("We have iterated through all the samples");
-            log::info!("Start outputting results");
+            log::info!("We have iterated through all the samples.");
+            log::info!("Start outputting results.");
             merge::count_subsample(prefix, all_samples, nodes, header);
+            log::info!("Congratulations, it's successful!");
+        }
+        Subcli::tovcf { ktable, prefix } => {
+            let header = "\
+##fileformat=VCFv4.2
+##source=kgwasV1.90
+##contig=<ID=0>
+##INFO=<ID=PR,Number=0,Type=Flag,Description=\"Provisional reference allele, may not be based on real reference genome\">
+##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n";
+            let header1 = "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT";
+            let output_file = format!("{}.vcf", prefix);
+            let mut file = File::create(output_file).expect("create output fail");
+            log::info!("Output vcf created successfully!");
+            file.write_all(header.as_bytes()).unwrap();
+
+            let f: File = File::open(&ktable).expect("open sample file error");
+            let reader = BufReader::new(f);
+
+            let first_line = reader.lines().next();
+            if let Some(Ok(header2)) = first_line {
+                let header_ok = format!("{}{}{}", header1, header2, "\n");
+                file.write_all(header_ok.as_bytes()).unwrap();
+            } else {
+                panic!("Error: Failed to read header2 from the file.");
+            }
+            log::info!("VCF header has been written to the file successfully.");
+
+            log::info!("Open kmer_table again.");
+            let f: File = File::open(ktable).expect("open sample file error");
+            let reader = BufReader::new(f);
+            let second_reader = reader.lines().skip(1);
+            log::info!("kmer_table convert to vcf ...");
+            for line in second_reader {
+                if let Ok(li) = line {
+                    let values: Vec<&str> = li.split_whitespace().collect();
+                    let chrom = "0";
+                    let pos = "0";
+                    let id = values[0];
+                    let ref_allele = "1";
+                    let alt_allele = ".";
+                    let qual = ".";
+                    let filter = ".";
+                    let info = "PR";
+                    let format_field = "GT";
+                    let new_line = format!(
+                        "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t",
+                        chrom, pos, id, ref_allele, alt_allele, qual, filter, info, format_field
+                    );
+                    let mut tem_value: Vec<&str> = Vec::new();
+                    for &value in values[1..].iter() {
+                        let allele = match value {
+                            "0" => "0/0",
+                            "1" => "1/1",
+                            _ => {
+                                panic!("open input file error");
+                            }
+                        };
+                        tem_value.push(allele);
+                    }
+                    let tem_value = tem_value.join("\t");
+                    file.write_all(format!("{}{}{}", new_line, tem_value, "\n").as_bytes())
+                        .unwrap();
+                }
+            }
+
             log::info!("Congratulations, it's successful!");
         }
         _ => {
